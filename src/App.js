a1c2c1d6f9c758f52d92f525f41b1f20dfa98816
Israel Zambrana <zambrana90@gmail.com>
2021-08-25 21:05:53 +0200

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <h1>Bulma</h1>
+    <div className="App">
+      <Navbar />
+    </div>
   );
 }
 
