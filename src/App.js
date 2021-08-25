f74bc1a31d83251d4df0f5d2cb12b4b37c34167d
Alejandra Rubio <alejandrarubio@MacBook-Air-de-Alberto-2.local>
2021-08-25 21:47:52 +0200

    iterations 1 and half

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from "./components/navbar/Navbar";
+import React from "react";
 
-function App() {
-  return (
-    <h1>Bulma</h1>
-  );
+export default class App extends React.Component {
+  render() {
+    return(
+      <div className="App">
+        <Navbar />
+      </div>
+    )
+  }
 }
-
-export default App;
