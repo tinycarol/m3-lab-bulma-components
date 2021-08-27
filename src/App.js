b39a337b48649b01a4d401b386a1073bb72802b0
Marcos Salvo <marcossalvo95@gmail.com>
2021-08-27 18:53:08 +0200

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import './App.css';
-import Navbar from "./components/navbar/Navbar"
-import FormField from "./components/formfield/FormField"
-import CoolButton from "./components/coolbutton/CoolButton"
+import Signup from "./components/signup/Signup"
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <div id="form">
-        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
-      </div>
-      <div className="buttons">
-        <CoolButton isRounded isDanger>
-          Button 1
-        </CoolButton>
-        <CoolButton isSuccess>
-          Button 2
-        </CoolButton>
-      </div>
-    </div>
+    <Signup />
   );
 }
 
--- src/components/coolbutton/CoolButton.js
+++ src/components/coolbutton/CoolButton.js
@@ -2,7 +2,7 @@ import "./CoolButton.css"
 
 let CoolButton = (props) => {
     return (
-        <button className={`button ${props.isRounded ? "is-rounded" : ""} ${props.isDanger ? "is-danger" : ""} ${props.isPrimary ? "is-primary" : ""} ${props.isSuccess ? "is-success" : ""}`}>{props.children}</button>
+        <button className={`button ${props.isRounded ? "is-rounded" : ""} ${props.isDanger ? "is-danger" : ""} ${props.isPrimary ? "is-primary" : ""} ${props.isSuccess ? "is-success" : ""} spacing`}>{props.children}</button>
     )
 }
 
