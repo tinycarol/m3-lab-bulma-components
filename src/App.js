4d936ee7ab8d97cc67253d37faf80ce238a2875e
Andrei <andrei_1002@hotmail.com>
2021-08-28 12:31:19 +0200

    finished till iteration 2, form field & navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import React from "react";
+import Navbar from './components/navbar/Navbar';
+import FormField from './components/formfield/FormField';
 
-function App() {
-  return (
-    <h1>Bulma</h1>
-  );
+const name = {
+  label: "Name",
+  placeholder: "e.g Alex Smith"
+};
+
+const email = {
+  label: 'Email',
+  placeholder: "e.g. alexsmith@gmail.com"
 }
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <FormField field={name} />
+        <FormField field={email} />
+      </div>
+    );
+  }
+}
