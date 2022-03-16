d656423f54b7c417e849f435b30bcc1843722e53
Maria Gallego <mariagestionredes@gmail.com>
2022-03-16 21:30:11 +0100

    iteration 1 done

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar is-transparent">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io" />
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: a modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+
+          <div
+            className="navbar-burger"
+            data-target="navbarExampleTransparentExample"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div id="navbarExampleTransparentExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item" href="https://bulma.io/">
+              Home
+            </a>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+       
+                <p className="control">
+                  <a
+                    className="button is-info"
+                    href="https://github.com/jgthms/bulma/releases/download/0.9.3/bulma-0.9.3.zip"
+                  >
+                    <span className="icon">
+                      <i className="fas fa-download"></i>
+                    </span>
+                    <span>Login</span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+       
+                <p className="control">
+                  <a
+                    className="button is-primary"
+                    href="https://github.com/jgthms/bulma/releases/download/0.9.3/bulma-0.9.3.zip"
+                  >
+                    <span className="icon">
+                      <i className="fas fa-download"></i>
+                    </span>
+                    <span>Signup</span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
