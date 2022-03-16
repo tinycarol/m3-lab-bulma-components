f9f4ca1be7c79ffaa92d4c38a79541f3a205a80e
Isa Gutierrez <isagr_kio@hotmail.com>
2022-03-16 21:34:25 +0100

    Navbar and Form done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Navbar from './components/nav/Navbar';
+import FormField from './components/formfield/FormField'
 
 function App() {
   return (
-    <h1>Bulma</h1>
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+    </div>
   );
 }
 
