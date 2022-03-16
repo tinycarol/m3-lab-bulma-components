bd17a4c3488afbed1889537a3c9c4158ae309280
Eduardo Francos Margareto <efmargareto@hotmail.com>
2022-03-16 21:33:55 +0100

    nav & formfiled componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar.js'
+import FormField from './components/formfield/Formfield';
 
 function App() {
   return (
-    <h1>Bulma</h1>
+    <>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+    </>
   );
 }
 
