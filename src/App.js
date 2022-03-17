3a9adc30cb802b21ba4ab913e1fb67afd352f3c7
AlejandroGC1990 <alejandro.garcia.carmona@gmail.com>
2022-03-17 04:49:21 +0100

    Iteration2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/navbar/navbar/Navbar'
+import FormField from './components/navbar/formfield/FormField';
+
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith"/>
+      <FormField label="Email" type="email" placeholder="e.g alexsmith@gmail.com"/>
+    </div>
   );
 }
 
---
+++ src/components/navbar/formfield/FormField.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./FormField.css";
+
+/*CON EL SIGUIENTE MÉTODO NO ME CARGA LA PÁGINA
+class FormField extends React.Component {
+    render() {
+        return (
+            <div>
+                <FormField 
+                    label="Name" 
+                    type="text" 
+                    placeholder="e.g Alex Smith"
+                />
+                <FormField 
+                    label="Email" 
+                    type="email" 
+                    placeholder="e.g. alexsmith@gmail.com"
+                /> 
+            </div>
+        )
+    }
+}*/
+
+class FormField extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="Formfield">
+          <label className="label">
+            {this.props.label}
+          </label>
+          <div className="control">
+            <input 
+              className="input" 
+              type="text"
+              placeholder={this.props.label} 
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default FormField;
