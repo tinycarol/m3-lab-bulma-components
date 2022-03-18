233f3e5b1dd3299b5d3466d22157f54c0ca3696c
Pablo Mateos <pmateos805@gmail.com>
2022-03-18 22:10:57 +0100

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import FormField from './components/FormField/FormField'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <div className='wrapper'>
+      <div className='forms'>
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      </div>
+    </div>
+
     </>
   );
 }
