a0bc4def4cac0fc8f7ad986001c52768d5dc4aec
David <davidblancoferrandez@gmail.com>
2022-03-19 00:41:13 +0100

    first commit

--- src/components/CoolButton/CoolButton.js
+++ src/components/CoolButton/CoolButton.js
@@ -1,33 +1,5 @@
 import React from "react";
-
-const buttonTypes = {
-	isActive: "is-active",
-	isBlack: "is-black",
-	isCentered: "is-centered",
-	isDanger: "is-danger",
-	isDark: "is-dark",
-	isFocused: "is-focused",
-	isGrouped: "is-grouped",
-	isHovered: "is-hovered",
-	isInfo: "is-info",
-	isInverted: "is-inverted",
-	isLarge: "is-large",
-	isLight: "is-light",
-	isLink: "is-link",
-	isLoading: "is-loading",
-	isMedium: "is-medium",
-	isOutlined: "is-outlined",
-	isPrimary: "is-primary",
-	isRight: "is-right",
-	isRounded: "is-rounded",
-	isSelected: "is-selected",
-	isSmall: "is-small",
-	isStatic: "is-static",
-	isSuccess: "is-success",
-	isText: "is-text",
-	isWarning: "is-warning",
-	isWhite: "is-white",
-};
+import { buttonTypes } from "./ButtonTypes";
 
 const CoolButton = (props) => {
 	return (
@@ -39,11 +11,11 @@ const CoolButton = (props) => {
 
 const getButtonTypes = (lst) => {
 	return Object.keys(lst)
-    .map((el) => {
-      if (el in buttonTypes) {
-        return buttonTypes[el]
-      }
-    })
+		.map((el) => {
+			if (el in buttonTypes) {
+				return buttonTypes[el];
+			}
+		})
 		.join(" ");
 };
 
