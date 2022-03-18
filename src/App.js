a53d975f18b3ed53fad2ffc6615024cad907abe9
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-03-18 17:31:48 +0100

    finish bonus iteration 5 and DONE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import Signup from './components/Signup/Signup.js';
+import Signup from './components/Signup/Signup';
+import Message from './components/Message/Message';
 import './App.css';
 
 function App() {
   return (
-    <Signup />
+    <>
+      <div className="container">
+        <Signup />
+      </div>
+      <div id="messages-div" className="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+        <div className="container mt-6 columns">
+          <div className="column is-6">
+            <Message color="is-primary" title="First Message">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vulputate fringilla ligula, vitae tincidunt sem porttitor eu.
+            </Message>
+          </div>
+          <div className="column is-6">
+            <Message color="is-warning" title="Second Message">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vulputate fringilla ligula, vitae tincidunt sem porttitor eu.
+            </Message>
+          </div>
+        </div>
+        <div className="container columns mb-6">
+          <div className="column is-6">
+            <Message color="is-danger" title="Third Message">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vulputate fringilla ligula, vitae tincidunt sem porttitor eu.
+            </Message>
+          </div>
+          <div className="column is-6">
+            <Message color="is-info" title="Fourth Message">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vulputate fringilla ligula, vitae tincidunt sem porttitor eu.
+            </Message>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
