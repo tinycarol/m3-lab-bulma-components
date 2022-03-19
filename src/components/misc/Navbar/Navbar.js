f931e67d708626726917a2bccea9ac772604ef56
Sergio Villabrille <sergiovillabrille@hotmail.com>
2022-03-19 12:43:42 +0100

    indentated, lab donete

--- src/components/misc/Navbar/Navbar.js
+++ src/components/misc/Navbar/Navbar.js
@@ -7,23 +7,18 @@ return(
     <a className="navbar-item" href="https://bulma.io">
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
     </a>
-
     <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-
   <div id="navbarBasicExample" className="navbar-menu">
     <div className="navbar-start">
       <a className="navbar-item">
         Home
       </a>
-
-
     </div>
-
     <div className="navbar-end">
       <div className="navbar-item">
         <div className="buttons">
